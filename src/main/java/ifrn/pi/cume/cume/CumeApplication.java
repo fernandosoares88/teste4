b1328ed0d2fa8efd4e637a3a47d9cdbc668c9615
Fernando Soares <fernandosoares@MacBook-Air-de-Fernando.local>
2024-10-03 09:14:33 -0300

    inicializando repositório

---
+++ src/main/java/ifrn/pi/cume/cume/CumeApplication.java
@@ -0,0 +1,13 @@
+package ifrn.pi.cume.cume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifrn/pi/cume/cume/CumeApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.pi.cume.cume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
